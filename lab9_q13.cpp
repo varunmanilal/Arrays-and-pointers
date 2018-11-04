bf7c527ffa23a7f4ca087592b9c04fedc95c2b41
Varun <42139581+varunmanilal@users.noreply.github.com>
2018-11-04 22:43:26 +0530

    Update lab9_q13.cpp

--- lab9_q13.cpp
+++ lab9_q13.cpp
@@ -1,11 +1,14 @@
+//include the library file
 #include<iostream>
 using namespace std;
-
+//the main function
 int main(){
 	char chuck[10]="baseballs";
+	//using indices
 	cout<<"Printing array 'chuck' using array indices: ";
         cout<<chuck;
 	cout<<endl;
+	//using pointers
 	cout<<"Printing array 'chuck' using pointers: ";
 	char*p=chuck;
 	for(int i=0;i<10;i++){
